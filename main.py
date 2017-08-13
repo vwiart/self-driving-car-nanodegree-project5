82421dfe39f42ebce79cfc954d95b9b7c7bb7094
Vincent Wiart <vwiart@users.noreply.github.com>
2017-08-13 15:05:11 +0200

    Revert "hog"

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from pipeline.image_features import test_color_space_features
-
+from pipeline.color_features import test_color_space_features
 
 def main():
     test_color_space_features()
-
 
 if __name__ == '__main__':
     main()
